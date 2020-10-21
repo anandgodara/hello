39e3766913fd2ebe53fe34c3e58b5ffd9a838a2d
Sachin Sharma <67816867+Sachinsharma01@users.noreply.github.com>
2020-10-21 21:16:35 +0530

    More user freindly and readable

--- Firstwork.java
+++ Firstwork.java
@@ -9,26 +9,29 @@ import java.util.Scanner;
 public class Firstwork {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
         String stuName = scn.next();
+        System.out.println("Please enter your father's name: ");
         String fatName = scn.next();
+        System.out.println("Please enter your mother's name: ");
         String motName = scn.next();
+        System.out.println("Please enter address: ");
         String address = scn.next();
+        System.out.println("Please enter your contact No. : ");
         long stuContNo = scn.nextLong();
+        System.out.println("Please enter your father's contact No. : ");
         long fatContNo = scn.nextLong();
+        System.out.println("Please enter your mother's contact No. : ");
         long motContNo = scn.nextLong();
+        System.out.println("Please enter your percentage in 12th: ");
         double perInTwelve = scn.nextDouble();
+        System.out.println("Please enter your percentage in 10th: ");
         double perInTen = scn.nextDouble();
+        System.out.println("Please enter your hostChoice: ");
         boolean hostChoice = scn.nextBoolean();
         scn.close();
-        System.out.println("Please enter your name: "+ stuName);
-        System.out.println("Please enter your father's name: "+ fatName);
-        System.out.println("Please enter your mother's name: "+ motName);
-        System.out.println("Please enter your permanent address: "+ address);
-        System.out.println("Please enter your contact number: "+ stuContNo);
-        System.out.println("Please enter your father's contact number: "+ fatContNo);
-        System.out.println("Please enter you mother's contact number: "+ motContNo);
-        System.out.println("Percentage in 12th: "+ perInTwelve);
-        System.out.println("Percentage in 10th: "+ perInTen);
-        System.out.println("Hostel Choice: "+ perInTen);
+        System.out.println("Name : " + stuName + "\nFathre's Name : " + fatName + "\nMother's Name : " + motName + "\nAddress : "
+         + address + "\nContact No : " + stuContNo + "\nFather's No : " + fatContNo + "\nMother's No : " + motContNo + "\n12th Precentage : " + 
+         perInTwelve + "\n10th Percentage : " + perInTen + "\nHost Choice : " + hostChoice);
     }
 }
